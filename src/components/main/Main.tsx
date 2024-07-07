13a7eb3f84f98fbd5dcfa0ac95032e3bc4cd7ae6
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-07-08 01:44:34 +0200

    feat: add style main component

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -4,6 +4,12 @@ import styles from './styles.module.scss';
 
 export class Main extends Component {
   public render(): ReactNode {
-    return <main className={styles.main}>this main</main>;
+    return (
+      <main className={styles.main}>
+        <div className={styles.card}>1</div>
+        <div className={styles.card}>2</div>
+        <div className={styles.card}>3</div>
+      </main>
+    );
   }
 }
