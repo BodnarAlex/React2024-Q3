8874b27e50b9b61ffdcc65211b6618cc1d70d5dc
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-07-08 12:59:48 +0200

    feat: add separetely main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,10 @@ import './App.css';
 import type { ReactNode } from 'react';
 import { Component } from 'react';
 
-import { Header } from '@/components/header/Header';
-import { Main } from '@/components/main/Main';
-import { Footer } from '@/components/footer/Footer.tsx';
+import { MainPage } from './pages/main-page/MainPage.tsx';
 
 export class App extends Component {
   public render(): ReactNode {
-    return (
-      <>
-        <Header />
-        <Main />
-        <Footer />
-      </>
-    );
+    return <MainPage />;
   }
 }
---
+++ src/pages/main-page/MainPage.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode } from 'react';
+import { Component } from 'react';
+
+import { Header } from '../../components/header/Header';
+import { Main } from '../../components/main/Main';
+import { Footer } from '../../components/footer/Footer.tsx';
+
+export class MainPage extends Component {
+  public render(): ReactNode {
+    return (
+      <>
+        <Header />
+        <Main />
+        <Footer />
+      </>
+    );
+  }
+}
