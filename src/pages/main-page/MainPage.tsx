4e5d3e394716f61eb31caf99b1d4003bbcf86e1a
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-07-13 18:15:18 +0200

    refactor: change class conponents on function for pages

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,32 +1,21 @@
-import type { ReactNode } from 'react';
-import { Component } from 'react';
+import { type ReactNode, useState } from 'react';
 
 import { Header } from '../../components/header/Header';
 import { Main } from '../../components/main/Main';
 import { Footer } from '../../components/footer/Footer.tsx';
-import type { IMainPageProps, IMainPageState } from './type.ts';
 
-export class MainPage extends Component<IMainPageProps, IMainPageState> {
-  constructor(props: IMainPageProps) {
-    super(props);
-    this.state = {
-      searchValue: '',
-    };
-  }
+export function MainPage(): ReactNode {
+  const [searchValue, setSearchValue] = useState<string>('');
 
-  private handleSearchChange = (value: string): void => {
-    this.setState({ searchValue: value });
+  const handleSearchChange = (value: string): void => {
+    setSearchValue(value);
   };
 
-  public render(): ReactNode {
-    const { searchValue } = this.state;
-
-    return (
-      <>
-        <Header onSearchChange={this.handleSearchChange} />
-        <Main searchValue={searchValue} />
-        <Footer />
-      </>
-    );
-  }
+  return (
+    <>
+      <Header onSearchChange={handleSearchChange} />
+      <Main searchValue={searchValue} />
+      <Footer />
+    </>
+  );
 }
