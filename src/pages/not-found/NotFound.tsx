aaf62a886e86562f9eb989c6e736a603be531860
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-07-13 20:47:06 +0200

    feat: add 404 page

---
+++ src/pages/not-found/NotFound.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode } from 'react';
+import styles from './styles.module.scss';
+
+export function NotFound(): ReactNode {
+  return (
+    <main className="main">
+      <div className={styles.mainError}>
+        <h1 className={styles.title}>This is</h1>
+        <div className={styles.dart}>
+          <div className={styles.error}>404</div>
+        </div>
+        <button className={styles.refreshBtn} type="button">
+          Return on other side
+        </button>
+      </div>
+    </main>
+  );
+}
