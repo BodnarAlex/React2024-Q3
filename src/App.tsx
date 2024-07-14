f51380fa543c9b8bfb889d5fa4bc0f30fae92499
Alexsandra Bodnar <66698314+BodnarAlex@users.noreply.github.com>
2024-07-14 09:03:09 +0200

    feat: add routing setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import type { ReactNode } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { MainPage } from './pages/main-page/MainPage.tsx';
+import { NotFound } from './pages/not-found/NotFound.tsx';
 
 export function App(): ReactNode {
-  return <MainPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/:page" element={<MainPage />} />
+      <Route path="/:page/:id" element={<NotFound />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './App.tsx';
 import { ErrorBoundary } from './components/error-boundary/ErrorBoundary.tsx';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ErrorBoundary>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ErrorBoundary>,
 );
